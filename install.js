ee08646329b8060cf4db46b198183c6346e5ba39
6Morpheus6 <wanton-decay@gmx.net>
2025-06-24 09:17:01 +0200

    prebuilt wheels, nvdiffrast patch

--- install.js
+++ install.js
@@ -30,6 +30,37 @@ module.exports = {
       }
     },
     {
+      when: "{{platform === 'win32' && gpu === 'nvidia' && kernel.gpus && kernel.gpus.find(x => / 50.+/.test(x.model))}}",
+      method: "shell.run",
+      params: {
+        env: {},
+        venv: "env",
+        path: "app",
+        message: [
+          "uv pip install https://github.com/MiroPsota/torch_packages_builder/releases/download/pytorch3d-0.7.8%2B366eff2/pytorch3d-0.7.8+366eff2pt2.7.0cu128-cp310-cp310-win_amd64.whl",
+          "uv pip install https://data.pyg.org/whl/torch-2.7.0%2Bcu128/torch_scatter-2.1.2%2Bpt27cu128-cp310-cp310-win_amd64.whl",
+          "uv pip install -r ../requirements.txt",
+          "copy /Y ..\\nvdiffrast\\ops.py env\\Lib\\site-packages\\nvdiffrast\\torch\\"
+        ]
+      },
+      next: "model"
+    },
+    {
+      when: "{{platform === 'linux' && gpu === 'nvidia' && kernel.gpus && kernel.gpus.find(x => / 50.+/.test(x.model))}}",
+      method: "shell.run",
+      params: {
+        env: {},
+        venv: "env",
+        path: "app",
+        message: [
+          "uv pip install https://github.com/MiroPsota/torch_packages_builder/releases/download/pytorch3d-0.7.8/pytorch3d-0.7.8+pt2.7.0cu128-cp310-cp310-linux_x86_64.whl",
+          "uv pip install https://data.pyg.org/whl/torch-2.7.0%2Bcu128/torch_scatter-2.1.2%2Bpt27cu128-cp310-cp310-linux_x86_64.whl",
+          "uv pip install -r ../requirements.txt"
+        ]
+      },
+      next: "model"
+    },
+    {
       method: "shell.run",
       params: {
         build: true,
@@ -40,11 +71,14 @@ module.exports = {
         path: "app",
         message: [
           "uv pip install -U setuptools",
-          "uv pip install -r ../requirements.txt --no-build-isolation"
+          "uv pip install -r ../requirements.txt",
+          "uv pip install git+https://github.com/facebookresearch/pytorch3d.git@stable --no-build-isolation",
+          "uv pip install torch_scatter --no-build-isolation"
         ]
       }
     },
     {
+      id: "model",
       method: "hf.download",
       params: {
         "_": [ "Wuvin/Unique3D" ],
